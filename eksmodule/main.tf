b97a35b8e5e6477622e2843902ac8f30bdff5bb6
AllanRodriques <allanrodriques2001@gmail.com>
2023-09-21 13:14:25 +0530

    updated tf

--- eksmodule/main.tf
+++ eksmodule/main.tf
@@ -1,23 +1,82 @@
-module "aws_eks_cluster" {
-
-  source = "./modules/aws_eks"
-
-  for_each = var.aws_eks_cluster_config
-
-  eks_cluster_name = each.value.eks_cluster_name
-  subnet_ids       = each.value.eks_subnet_ids
-  tags             = each.value.tags
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAV57FEFNQLJ3K2RB7"
+  secret_key = "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"
 }
 
-module "aws_eks_node_group" {
 
-  source = "./modules/aws_eks_nodegroup"
+data "aws_availability_zones" "available" {}
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
 
-  for_each = var.eks_node_group_config
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
 
-  node_group_name  = each.value.node_group_name
-  eks_cluster_name = module.aws_eks_cluster[each.value.eks_cluster_name].eks_cluster_name
-  subnet_ids       = each.value.node_subnet_ids
-  nodes_iam_role   = each.value.nodes_iam_role
-  tags             = each.value.tags
+locals {
+  cluster_name = "java-app"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+module "eks-kubeconfig" {
+  source       = "hyperbadger/eks-kubeconfig/aws"
+  version      = "2.0.0"
+  cluster_name = "java-app"
+  depends_on   = [module.eks]
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.eks-kubeconfig.kubeconfig
+  filename = "kubeconfig_${local.cluster_name}"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name                 = "k8s-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.16.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
+  subnet_ids      = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 5
+      min_capacity     = 1
+
+      instance_type = "m5.large"
+    }
+  }
 }
